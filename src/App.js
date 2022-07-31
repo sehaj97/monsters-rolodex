53ef2380c65ecccda1cd7576e86eb54f6d5a1cbe
Sehajpreet Singh <sehajmagan@gmail.com>
2022-07-30 22:35:02 -0400

    testing events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("magan");
+  function handleClick() {
+    name === "magan" ? setName("sehaj") : setName("magan");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{name}</p>
+        <button onClick={handleClick}>Change Name</button>
       </header>
     </div>
   );
